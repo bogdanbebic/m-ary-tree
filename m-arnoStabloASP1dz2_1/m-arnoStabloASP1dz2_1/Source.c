62bbae6d9ad3db4d04a5e51fd504cabde82df902
bogdanbebic <bb170011d@student.etf.bg.ac.rs>
2018-04-06 13:34:08 +0200

    Implemented stack

--- m-arnoStabloASP1dz2_1/m-arnoStabloASP1dz2_1/Source.c
+++ m-arnoStabloASP1dz2_1/m-arnoStabloASP1dz2_1/Source.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct TreeNodeStruct {
 	int data;
@@ -8,8 +9,47 @@ typedef struct TreeNodeStruct {
 typedef struct TreeStruct {
 	TreeNode *root;
 	int maxNumberOfChildren;
+	int numberOfNodes;
 	int height;
 } Tree;
+
+#pragma region STACK_IMPLEMENTATION
+
+typedef struct StackTreeNodeStruct {
+	TreeNode *treeNode;
+	struct StackTreeNodeStruct *next;
+} StackTreeNode;
+
+StackTreeNode *allocateStackNode() {
+	StackTreeNode *node;
+	node = malloc(sizeof(StackTreeNode));
+	return node;
+}
+
+int push(StackTreeNode *stack, TreeNode *elem) {
+	StackTreeNode *node;
+	node = allocateStackNode();
+	if (node == NULL) {
+		return 0;
+	}
+	node->treeNode = elem;
+	node->next = stack;
+	return 1;
+}
+
+TreeNode *pop(StackTreeNode *stack) {
+	StackTreeNode *old;
+	TreeNode *elem = NULL;
+	if (stack != NULL) {
+		old = stack;
+		stack = stack->next;
+		elem = old->treeNode;
+		free(old);
+	}
+	return elem;
+}
+
+#pragma endregion
 
 int main() {
 
